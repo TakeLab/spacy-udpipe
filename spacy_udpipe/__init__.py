08e569cc43f2c049d5eb94e58266e082f72dce85
Antonio Šajatović <antonio.sajatovic@gmail.com>
2019-08-08 16:56:01 +0200

    Use __all__ for imports

--- spacy_udpipe/__init__.py
+++ spacy_udpipe/__init__.py
@@ -1,2 +1,5 @@
 from .language import UDPipeLanguage, UDPipeModel, load, load_from_path
 from .util import download
+
+__all__ = ["UDPipeLanguage", "UDPipeModel",
+           "load", "load_from_path", "download"]
