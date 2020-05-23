ae703270987a202c5ba2d3b1bf6ef66cd23c9ce5
Antonio Šajatović <antonio.sajatovic@gmail.com>
2020-05-23 17:08:36 +0200

    Fix __init__.py

--- spacy_udpipe/__init__.py
+++ spacy_udpipe/__init__.py
@@ -1,6 +1,6 @@
-from .language import UDPipeLanguage, UDPipeModel, load, load_from_path
-from .utils import download
-
+__version__ = "0.3.1"
 __all__ = ["UDPipeLanguage", "UDPipeModel",
            "load", "load_from_path", "download"]
-__version__ = "0.3.1"
+
+from .language import UDPipeLanguage, UDPipeModel, load, load_from_path
+from .utils import download
