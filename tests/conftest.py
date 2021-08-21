79c4c87fc1ed5367852f019aabec8949acb40f9b
mariosasko <mariosasko777@gmail.com>
2021-08-21 19:39:57 +0200

    Rename conftest function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def set_test_cache_config(tmp_path_factory, monkeypatch):
+def set_test_models_dir(tmp_path_factory, monkeypatch):
     test_models_dir = tmp_path_factory.mktemp("models")
     monkeypatch.setattr("spacy_udpipe.utils.MODELS_DIR", str(test_models_dir))
